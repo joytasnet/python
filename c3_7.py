d14e529badbfd4ed83db3f275700058870fa2a99
mjpurin <mjpurin@gmail.com>
2023-08-07 13:17:47 +0900

    第3章まで

---
+++ c3_7.py
@@ -0,0 +1,5 @@
+score=int(input('試験の点数を入力>>'))
+if 60 <= score <= 100:
+    print('ok')
+else:
+    print('ng')
---
+++ ex3_2.py
@@ -0,0 +1,21 @@
+initial = 'K'
+if initial == 'k':
+    print('ok')
+
+point = 82
+if 80 <= point < 256:
+    print('ok')
+
+bmi = 24
+if bmi < 20 or bmi > 25:
+    print('ok')
+
+year = 2023
+if year % 4 == 0:
+    print('ok')
+
+day = 27
+if day not in [28,30,31]:
+    print('day ok')
+
+
