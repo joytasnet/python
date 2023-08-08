f91f6b5a7b32ca3855cefc5997e994378ecde755
mjpurin <mjpurin@gmail.com>
2023-08-08 11:20:55 +0900

    第4章まで

---
+++ c4_7.py
@@ -0,0 +1,6 @@
+scores = [80,20,75,60]
+for n in scores:
+    if n >= 60:
+        print('合格')
+    else:
+        print('不合格')
---
+++ ex4_3.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print(f'{10-i}',end=',')
+print('Lift off!')
+
+for i in range(10,0,-1):
+    print(f'{i}',end=',')
+print('Lift off!')
+
+for i in range(3,10,2):
+    print(f'{i}',end=',')
+print('Lift off!')
+
